12f275408a83243bc0b8ad001a97cfdbd0a9ec36
HaPham <ngoha.pham1202@gmail.com>
2022-06-26 22:13:46 +0700

    upload ảnh

--- AppBanHang/app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ AppBanHang/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
-    //tạo link kết nối với database tại mysql
-    public static  final  String BASE_URL="http://192.168.31.248/banhang/";
+    //Tạo link kết nối với database tại mysql
+    public static  final  String BASE_URL="http://192.168.1.17/banhang/";
     //Tạo một danh sách lưu các sản phẩm tại giỏ hàng
     public static List<GioHang> manggiohang;
-    //lưu danh sách sản phẩm tại sẽ mua
+    //Lưu danh sách sản phẩm tại sẽ mua
     public static List<GioHang> mangmuahang= new ArrayList<>();
     //Lưu thông tin user
     public  static User user_curent = new User();
