68d13599cf91e04f8b4eb964aedc54b709464abb
HaPham <ngoha.pham1202@gmail.com>
2022-07-06 21:15:18 +0700

    STS-1000 add notification with firebase

--- AppBanHang/app/src/main/java/com/example/appbanhang/service/FirebaseMessagerRecevier.java
+++ AppBanHang/app/src/main/java/com/example/appbanhang/service/FirebaseMessagerRecevier.java
@@ -1,10 +1,17 @@
 package com.example.appbanhang.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.example.appbanhang.R;
+import com.example.appbanhang.activity.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -12,7 +19,30 @@ public class FirebaseMessagerRecevier extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+        if(message.getNotification() != null){
+            showNotification(message.getNotification().getTitle(),message.getNotification().getBody());
+        }
+    }
+
+    private void showNotification(String title, String body) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        String chanelId ="noti";
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),chanelId)
+                .setSmallIcon(R.drawable.ic_email)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000,1000,1000,1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+        builder = builder.setContent(customView(title,body));
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(chanelId,"web_app",NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        notificationManager.notify(0,builder.build());
+
     }
 
     private RemoteViews customView(String title, String body){
